9cf52beeecde14f079ca302c1f16961f5e6872da
Devon Wesley <dwesley89@gmail.com>
2016-10-03 16:43:46 -0700

    authors associated in DB

--- faker.js
+++ faker.js
@@ -5,19 +5,72 @@ const db = pgp( connectionString );
 
 const faker = require( 'faker' )
 
+
+const fakeAuthors = author => {
+  const sql = 'INSERT INTO authors( name, bio, img_url ) VALUES ( $1, $2, $3 )'
+  db.none( sql, [ author.name, author.bio, author.img_url ] )
+}
+
+const generateAuthors = () => {
+  for( let i = 30; i >= 0; i-- ) {
+    fakeAuthors({
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      bio: faker.lorem.paragraph(),
+      img_url: faker.image.people()
+    })
+  }
+}
+
 const fakeBook = book => {
   const sql = 'INSERT INTO books( title, description, img_url ) VALUES ( $1, $2, $3 )'
   db.none( sql, [ book.title, book.description, book.img_url ] )
 }
 
-const generate = () => {
+const generateBooks = () => {
   for( let i = 30; i >= 0; i-- ) {
     fakeBook({
-      title: faker.commerce.product(),
+      title: faker.internet.domainName(),
       description: faker.lorem.paragraph(),
       img_url: faker.image.people()
     })
   }
 }
 
-module.exports = { generate }
+const findBooks = () => {
+  const sql = 'SELECT * FROM books'
+  return db.any( sql )
+}
+
+const findAuthors = () => {
+  const sql = 'SELECT * FROM authors'
+  return db.any( sql )
+}
+
+const bookAuthors = ( ids ) => {
+  const sql = 'INSERT INTO book_authors( book_id, author_id ) VALUES ( $1, $2 )'
+
+  db.any( sql, [ ids.book_id, ids.author_id ])
+}
+
+const generateBookAuthors = () => {
+  findBooks().then( books => {
+    Promise.resolve( findAuthors() )
+      .then( authors => {
+        const queries = []
+        console.log( authors, books );
+
+        for( let i = 30; i >= 0; i-- ) {
+          queries.push(
+            bookAuthors({
+              book_id: faker.random.arrayElement( books ).id,
+              author_id: faker.random.arrayElement( authors ).id
+            })
+          )
+        }
+
+        Promise.all( queries )
+      })
+  })
+}
+
+module.exports = { generateAuthors,  generateBooks, generateBookAuthors }
