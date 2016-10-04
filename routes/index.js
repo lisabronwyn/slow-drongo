ea979cc62e568a8c2cc59cbec6e3f1dd4a01a472
Devon Wesley <dwesley89@gmail.com>
2016-10-04 10:19:51 -0700

    book and author query route

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,18 @@ const { Book } = require('../database/db')
 
 /* GET home page. */
 router.get('/', ( request, response ) => {
-
   Book.getAll().then( books => response.render( 'index', { books } ) )
+})
+
+router.get('/book/:book_id', ( request, response ) => {
+  const { book_id } = request.params
+
+  Promise.all([ Book.getBook( book_id ), Book.getAuthor( book_id ) ])
+    .then( data => {
+      const [ book, author ] = data
+
+      response.send( data )
+    })
 })
 
 module.exports = router
