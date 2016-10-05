81b24f6047644ecbf227085bd7e97e8cb722f881
Max Antinone <mantinone@gmail.com>
2016-10-04 17:28:52 -0700

    add author details page and links

--- database/db.js
+++ database/db.js
@@ -4,8 +4,9 @@ const pgp = require('pg-promise')();
 const db = pgp( connectionString );
 
 const getAllBooks = 'SELECT * FROM books'
+const getAuthor = 'SELECT * FROM authors WHERE id=$1'
 const getBook = 'SELECT * FROM books WHERE id=$1'
-const getAuthor = `
+const getAuthorByBookID = `
   SELECT
     *
   FROM
@@ -17,11 +18,28 @@ const getAuthor = `
   WHERE
     book_authors.book_id=$1
   `;
+  const getBooksByAuthorID = `
+    SELECT
+      *
+    FROM
+      books
+    JOIN
+      book_authors
+    ON
+      books.id = book_authors.book_id
+    WHERE
+      book_authors.author_id=$1
+  `
 
 Book = {
   getAll: () => db.any( getAllBooks ),
   getBook: book_id => db.one( getBook, [ book_id ] ),
-  getAuthor: book_id => db.any( getAuthor, [ book_id ] )
+  getAuthor: book_id => db.any( getAuthorByBookID, [ book_id ] )
 }
 
-module.exports = { Book }
+Author = {
+  getAuthor: author_id => db.one( getAuthor, [ author_id]),
+  getBook: author_id => db.any( getBooksByAuthorID, [author_id])
+}
+
+module.exports = { Book, Author }
