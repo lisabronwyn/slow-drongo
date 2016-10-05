590e4cbeff24255043f2d889bffd365c8ddc8e53
Devon Wesley <dwesley89@gmail.com>
2016-10-05 15:10:16 -0700

    search with style

--- database/db.js
+++ database/db.js
@@ -18,10 +18,40 @@ const getAuthor = `
     book_authors.book_id=$1
   `;
 
-Book = {
+const Search = {
+  forBooks: search => {
+    const variables = []
+    let sql = `SELECT DISTINCT(books.*) FROM books`
+
+    if (search){
+      let search_query = search
+        .toLowerCase()
+        .replace(/^ */, '%')
+        .replace(/ *$/, '%')
+        .replace(/ +/g, '%')
+
+      variables.push( search_query )
+
+      sql += `
+      LEFT JOIN book_authors ON books.id=book_authors.book_id
+      LEFT JOIN authors ON authors.id=book_authors.author_id
+      LEFT JOIN book_genres ON books.id=book_genres.book_id
+      LEFT JOIN genres ON genres.id=book_genres.genre_id
+      WHERE LOWER(books.title)  LIKE $${variables.length}
+      OR LOWER(authors.name) LIKE $${variables.length}
+      OR LOWER(genres.title) LIKE $${variables.length}
+      ORDER BY books.id ASC
+      `
+    }
+
+    return db.any( sql, variables )
+  }
+}
+
+const Book = {
   getAll: () => db.any( getAllBooks ),
   getBook: book_id => db.one( getBook, [ book_id ] ),
   getAuthor: book_id => db.one( getAuthor, [ book_id ] )
 }
 
-module.exports = { Book }
+module.exports = { Book, Search }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const { Book } = require('../database/db')
+const { Book, Search } = require('../database/db')
 
 /* GET home page. */
 router.get('/', ( request, response ) => {
   Book.getAll().then( books => response.render( 'index', { books } ) )
+})
+
+router.get('/search-books', ( request, response ) => {
+  const { search_query } = request.query
+
+  Search.forBooks( search_query ).then( books => {
+    console.log( 'Second Stop', books );
+
+    response.send( books )
+  })
+  .catch( error => error )
 })
 
 router.get('/book/:book_id', ( request, response ) => {
